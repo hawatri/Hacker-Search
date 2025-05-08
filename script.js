06f2e00f2a5e6172bc3c3a834d314594993c25c1
hawatri <fardinrahaman28@gmail.com>
2025-05-09 00:00:59 +0530

    Changed settings/save

--- script.js
+++ script.js
@@ -684,10 +684,6 @@ function saveSettings() {
     applyTheme(settings.theme);
     toggleHeaders(settings.showHeaders);
     toggleAITools(settings.showAITools);
-    
-    // Close the settings modal
-    settingsModal.classList.remove('visible');
-    settingsModal.setAttribute('aria-hidden', 'true');
   });
 }
 
